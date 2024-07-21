24ba87516592d337973f637d90b507e068ce0188
AhmedHassan <Ahmedhassan@fci.helwan.edu.eg>
2024-07-21 14:34:06 +0300

    Creating Celebrat .fxml file and adding  mediaView , two Buttons  play Again and BackHome to WinningScreen class

--- src/sample/WinningScreen/WinningScreen.java
+++ src/sample/WinningScreen/WinningScreen.java
@@ -1,4 +1,52 @@
 package sample.WinningScreen;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.MediaView;
 
-public class WinningScreen {
+public  class WinningScreen extends AnchorPane {
+
+    protected final MediaView mediaView;
+    protected final VBox vBox;
+    protected final Button playAgainButton;
+    protected final Button backHomeButton;
+
+    public WinningScreen() {
+
+        mediaView = new MediaView();
+        vBox = new VBox();
+        playAgainButton = new Button();
+        backHomeButton = new Button();
+
+        setMaxHeight(USE_PREF_SIZE);
+        setMaxWidth(USE_PREF_SIZE);
+        setMinHeight(USE_PREF_SIZE);
+        setMinWidth(USE_PREF_SIZE);
+        setPrefHeight(400.0);
+        setPrefWidth(600.0);
+
+        mediaView.setFitHeight(400.0);
+        mediaView.setFitWidth(600.0);
+
+        vBox.setLayoutX(493.0);
+        vBox.setLayoutY(116.0);
+        vBox.setPrefHeight(200.0);
+        vBox.setPrefWidth(100.0);
+
+        playAgainButton.setAlignment(javafx.geometry.Pos.CENTER_RIGHT);
+        playAgainButton.setMnemonicParsing(false);
+        playAgainButton.setText("Play Again");
+
+        backHomeButton.setAlignment(javafx.geometry.Pos.CENTER_RIGHT);
+        backHomeButton.setLayoutX(493.0);
+        backHomeButton.setLayoutY(285.0);
+        backHomeButton.setMnemonicParsing(false);
+        backHomeButton.setText("Back Home");
+
+        getChildren().add(mediaView);
+        vBox.getChildren().add(playAgainButton);
+        getChildren().add(vBox);
+        getChildren().add(backHomeButton);
+
+    }
 }
