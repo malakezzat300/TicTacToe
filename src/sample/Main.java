e2d02e7519fe97a32dc226f030a35f2a6b8574c3
mostafagamal-cell <76878167+mostafagamal-cell@users.noreply.github.com>
2024-07-27 01:25:21 +0300

    upates types.class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.NetworkPackge.ClientSocket;
+import sample.NetworkPackge.Network;
 
 public class Main extends Application {
 
@@ -14,6 +16,10 @@ public class Main extends Application {
         primaryStage.setTitle("Tic Tac Toe");
 
         primaryStage.setScene(new Scene(root,800, 800));
+        primaryStage.setOnCloseRequest(e->{
+            if (ClientSocket.getInstance()!=null)
+                ClientSocket.getInstance().closeConnections();
+        });
         primaryStage.show();
         primaryStage.setMinHeight(800);
         primaryStage.setMinWidth(800);
